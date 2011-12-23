e4cf38c5a3f61812a364b2de0346e0dcb2fc0af2
Giles Malet <gdmalet+github@gmail.com>
2011-12-23 03:23:51 -0500

    Fix type on config screen.

--- joystick.cpp
+++ joystick.cpp
@@ -970,7 +970,7 @@ INT_PTR CALLBACK ConfigDlgProc( HWND hDlg, UINT msg, WPARAM wParam, LPARAM lPara
 				GetWindowRect(hDlgParent, &rcParent);
 				if ( rcParent.left < 0 ) rcParent.left = 0;
 				if ( rcParent.right < 0 ) rcParent.right = 0;
-				sprintf(buf, "Positionn X %u,  Y %u;  Size %u x %u", rcParent.left, rcParent.top,
+				sprintf(buf, "Position X %u,  Y %u;  Size %u x %u", rcParent.left, rcParent.top,
 					rcParent.right - rcParent.left, rcParent.bottom - rcParent.top );
 				SetWindowText( GetDlgItem( hDlg, IDC_WINDOW_POSN ), buf );
 
